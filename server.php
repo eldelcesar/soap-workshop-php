2d934bf056a648d6f66ed6093c14ef21feb12c05
eldelcesar <cesarjr.espinosa@gmail.com>
2018-04-18 18:58:44 -0500

    SOAP with mysql connection

--- server.php
+++ server.php
@@ -5,11 +5,24 @@
       public function __construct()
       {
         $this->con = (is_null($this->con)) ? self::connect() : $this->con;
-      } 
+      }
+
+      static function connect()
+      {
+        $con = mysql_connect('localhost', 'root', 'root');
+        $dv = mysql_select_db('example', $con);
+
+        return $con;
+      }
       
       public function getStudentName($id_array)
       {
-          return 'Miguel';
+          $id = $id_array['id'];
+          $sql = "SELECT name FROM students WHERE id = '$id'";
+          $qry = mysql_query($sql, $this->con);
+          $res = mysql_fetch_array($qry);
+          return $res['name'];
+          //return 'Miguel';
       } 
     }
 
